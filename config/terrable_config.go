c1b83ed80292949aacfe75265d27bb53a3e0105c
Callum Evans <callumevans@outlook.com>
2025-06-24 00:41:03 +0100

    Simplify env vars (#86)

--- config/terrable_config.go
+++ config/terrable_config.go
@@ -1,16 +1,15 @@
 package config
 
 type TerrableConfig struct {
-	Handlers                   []HandlerMapping
-	GlobalEnvironmentVariables map[string]string
-	Timeout                    int
+	Handlers             []HandlerMapping
+	EnvironmentVariables map[string]string
+	Timeout              int
 }
 
 type HandlerMapping struct {
-	Name                 string
-	Source               string
-	Http                 map[string]string
-	Sqs                  map[string]interface{}
-	EnvironmentVariables map[string]string
-	Timeout              int
+	Name    string
+	Source  string
+	Http    map[string]string
+	Sqs     map[string]interface{}
+	Timeout int
 }
